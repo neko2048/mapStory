5b91f4b4b009a8ee92136d6d6598ac71cb3fd225
neko2048 <show.999991@gmail.com>
2024-02-18 01:53:21 +0800

    (Feat) Add formatDate function and display map description and creation date

--- client/src/pages/MapDetail.js
+++ client/src/pages/MapDetail.js
@@ -18,10 +18,24 @@ export default function MapDetail(){
         setMap(res.data.data);
     };
 
+    const formatDate = (date) => {
+        let d = new Date(date),
+            year = d.getFullYear(),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate();
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        return [year, month, day].join('/');
+    };
+
     return (
         <div className="">
             <Header />
             <p className="text-center text-3xl mt-20">The post ID is {id}, {map.mapName}</p>
+            <p className="text-center text-3xl mt-20">Description: {map.mapDescription}</p>
+            <p className="text-center text-3xl mt-20">Create at: {formatDate(map.createdAt)}</p>
         </div>
     );
 }
