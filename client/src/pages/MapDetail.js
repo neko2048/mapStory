2c0eb8df93416af0ec911e494ae2c5960f804e52
neko2048 <show.999991@gmail.com>
2024-03-18 17:15:24 +0800

    (Feat) Add NaviBar component draft

--- client/src/pages/MapDetail.js
+++ client/src/pages/MapDetail.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { useParams } from "react-router-dom";
 import Header from '../components/Header';
 import MapView from '../components/MapView';
+import NaviBar from '../components/naviBar';
 
 
 export default function MapDetail(){
     return (
         <div>
             <div><Header /></div>
-            <div><MapView /></div>
+            <div style={{ display: 'flex' }}>
+                <div style={{ flex: 0.2 }}><NaviBar /></div>
+                <div style={{ flex: 1 }}><MapView /></div>
+            </div>
         </div>
     );
 }
